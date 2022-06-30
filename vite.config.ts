78a7a400bb8e608e3c80e54ce46aff53ccc429eb
uxdx <uxdx@naver.com>
2022-06-30 22:24:29 +0900

    ''

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,7 @@
-import { build, defineConfig } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    outDir: './public'
-  }
+  plugins: [react()]
 })
