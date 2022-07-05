83e0d3118533d1b7726c176b05d81852b00a7639
uxdx <uxdx@naver.com>
2022-07-06 01:28:15 +0900

    adminpage new post;

--- src/pages/AdminPage.tsx
+++ src/pages/AdminPage.tsx
@@ -1,33 +1,83 @@
-import { useState } from "react";
+import { ReactNode, useState } from "react";
+import { theme } from "../Settings";
 import styled from "styled-components";
-import PostsSetting from "./adminpage/PostsSetting";
+import MDEditor from '@uiw/react-md-editor';
 
-const Page = styled.div`
-
+const Admin = styled.div`
+display: flex;
+background-color: ${theme.primary};
+color: ${theme.on_p};
+min-height: 1000px;
 `
 const MenuBar = styled.div`
+background-color: ${theme.p_light};
+color: ${theme.on_p};
+width: 220px;
+.menu-item{
+    height: 60px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    cursor: pointer;
+    .icon{
+        font-size: 32px;
+    }
+}
+`
+const Page = styled.div`
 `
 
 function AdminPage(){
-    let [currentMenu, setCurrentMenu] = useState(0);
-    const menuTitle = ["Posts", "Menu2"];
-    const menus = [<PostsSetting/>, <div> Menu2</div>];
+    const [page, setPage] = useState<ReactNode>(<div>default</div>);
     return (
-        <Page>
+        <Admin>
             <MenuBar>
-                
+                <div className="menu-item" onClick={()=>{
+                    setPage(<NewPostPage/>);
+                }}>
+                    {/* 글쓰기 */}
+                    <span className="material-symbols-outlined icon">
+                        edit_note
+                    </span>
+                    <span>
+                        글 쓰기
+                    </span>
+                </div>
+                <div className="menu-item" onClick={()=>{
+                    setPage(<PostListPage/>);
+                }}>
+                    <span className="material-symbols-outlined icon">
+                        list_alt
+                    </span>
+                    <span>
+                        글 목록
+                    </span>
+                </div>
             </MenuBar>
-        </Page>
+            <Page>
+                {page}
+            </Page>
+        </Admin>
     );
 }
-function AdminSidebarItem(props:{text:string, onClick:Function}){
-    return (
-        <div className="admin-sidebar-item">
-            <button onClick={()=>{
-                props.onClick();
-            }}>
-                {props.text}
-            </button>
+
+function NewPostPage(){
+    const [value, setValue] = useState<string | undefined>("**Hello world!!!**");
+    return(
+        <div>
+            <MDEditor
+                value={value}
+                onChange={setValue}
+            />
+            <MDEditor.Markdown source={value} style={{ whiteSpace: 'pre-wrap' }} />
+        </div>
+    );
+}
+
+function PostListPage(){
+    return(
+        <div>
+            post list
         </div>
     );
 }
